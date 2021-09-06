56d18e494c298eddcac5c03a054e6bb9b839b222
Serhii Kovalov <andrey_roland@hotmail.com>
2021-09-07 01:47:28 +0300

    changed front in manage, post create views

--- Photo_album/App_Start/NinjectWebCommon.cs
+++ Photo_album/App_Start/NinjectWebCommon.cs
@@ -1,17 +1,16 @@
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Photo_album.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Photo_album.App_Start.NinjectWebCommon), "Stop")]
+using System;
+using System.Web;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
+using Photo_album;
 
-namespace Photo_album.App_Start
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+
+namespace Photo_album
 {
-    using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-    using Ninject.Web.Common.WebHost;
-
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
