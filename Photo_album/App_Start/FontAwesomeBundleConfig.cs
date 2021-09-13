1e8100ae36c7d86c38feed82a818e8bb90a49654
Serhii Kovalov <andrey_roland@hotmail.com>
2021-09-13 23:08:20 +0300

    added web config changes

--- Photo_album/App_Start/FontAwesomeBundleConfig.cs
+++ Photo_album/App_Start/FontAwesomeBundleConfig.cs
@@ -11,7 +11,7 @@ namespace Photo_album
 		{
 			// Add @Styles.Render("~/Content/fontawesome") in the <head/> of your _Layout.cshtml view
 			// When <compilation debug="true" />, MVC will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/fontawesome").Include("~/Content/fontawesome/font-awesome.css"));
+			BundleTable.Bundles.Add(new StyleBundle("~/Content/fontawesome").Include("~/Content/fontawesome/font-awesome.min.css"));
 		}
 	}
 }
